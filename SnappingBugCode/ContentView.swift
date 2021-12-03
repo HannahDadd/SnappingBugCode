eb1b05b9e5b922df9b2c047e0c2caf1ebf3c17bd
Hannah Billingsley-Dadd <hannah.billingsley-dadd01@bbc.co.uk>
2021-12-03 11:47:01 +0000

    Initial Commit

---
+++ SnappingBugCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SnappingBugCode
+//
+//  Created by Hannah Billingsley-Dadd on 03/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SnappingBugCode/SnappingBugCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnappingBugCodeApp.swift
+//  SnappingBugCode
+//
+//  Created by Hannah Billingsley-Dadd on 03/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct SnappingBugCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
